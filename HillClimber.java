62c46bc26d917ac2c6a19fd1e062224a38a55de0
Tyler Degen <degen.19@osu.edu>
2017-04-04 23:56:55 -0400

    first commit

---
+++ HillClimber.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class HillClimber {
+	private HillClimber(){}
+	
+	public static void main(String args[]){
+		System.out.println("Test");
+	}
+}
