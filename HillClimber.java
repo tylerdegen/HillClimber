c671b614ccb49be3e1e165e24adf681fb0b35f1d
Tyler Degen <degen.19@osu.edu>
2017-04-05 16:17:41 -0400

    start of classes

--- HillClimber.java
+++ HillClimber.java
@@ -3,11 +3,33 @@ import java.lang.*;
 import java.util.*;
 
 public class HillClimber {
-	private HillClimber(){}
+	
+	public int[][] board = new int[4][4];
+
+	//init
+	public HillClimber(){
+		//board = {{1,2,3,4},{5,6,7,8},{1,2,3,4},{1,2,3,4}};
+		for (int i=0; i < 4; i++){
+			for (int j=0; j < 4; j++){
+				board[i][j] = j+1;
+			}
+		}
+	}
+	
+	public void printState(){
+		String line = "";
+		for (int i=0; i < 4; i++){
+			for (int j=0; j < 4; j++){
+				line+= board[i][j];
+			}
+			System.out.println(line);
+			line = "";
+		}
+	}
 	
 	public static void main(String args[]){
 		System.out.println("Test");
 		
-		int[][] board = new int[4][4];
+		
 	}
 }
---
+++ HillClimberTest.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class HillClimberTest {
+	private HillClimberTest(){}
+	
+	public static void main(String args[]){
+		System.out.println("Test");
+		
+		HillClimber h = new HillClimber();
+		h.printState();
+	}
+}
