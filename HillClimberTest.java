e8db8abe7cf6d48c6c1c7465fa6b87c01e8b2e7a
Tyler Degen <degen.19@osu.edu>
2017-04-05 23:48:16 -0400

    hill climber fully functional

--- HillClimberTest.java
+++ HillClimberTest.java
@@ -6,15 +6,19 @@ public class HillClimberTest {
 	private HillClimberTest(){}
 	
 	public static void main(String args[]){
-		System.out.println("Test");
+		//System.out.println("Test");
 		
-		HillClimber h = new HillClimber("test.txt");
+		String filename = args[0];
+		//System.out.println(filename);
+		HillClimber h = new HillClimber(filename);
+		System.out.println("Initial state: ");
 		h.printState();
 		System.out.println(h.checkState());
 		//h.climb();
 		
-		for(int i = 0; i < 50 || h.checkState() == 0; i++){
+		for(int i = 0; i < 500 && h.checkState() != 0; i++){
 			h.climb();
+			System.out.println("Iteration " + i + ": ");
 			h.printState();
 			System.out.println(h.checkState());
 		}
